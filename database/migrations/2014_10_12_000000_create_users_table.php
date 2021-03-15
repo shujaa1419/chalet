e230b8f9e5cb4107c8f99763c52b8c49d3ea7591
Mr78S <rm_abody@hotmail.com>
2021-03-15 17:45:07 +0300

    edit readme.md and add ERD image

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
+	
     /**
      * Run the migrations.
      *
@@ -23,7 +24,7 @@ class CreateUsersTable extends Migration
             $table->timestamps();
         });
     }
-
+	
     /**
      * Reverse the migrations.
      *
