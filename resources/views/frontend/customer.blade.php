cc8e86bf43887de8fdbc8914eeceb977473651a8
shujaa1419 <shjoo1419@gmail.com>
2021-04-03 22:16:39 +0300

    add customer.blade

---
+++ resources/views/frontend/customer.blade.php
@@ -0,0 +1,99 @@
+@extends('layouts.app')
+@section('content')
+    <section class="wn_contact_area bg--white pt--80 pb--80">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-12">
+                    <div class="contact-form-wrap">
+                        <h2 class="contact__title">Reserve your own chalet</h2>
+                        <p></p>
+                        <form id="contact-form" action="{{route('chalet.frontend.add_customer')}}" method="post">
+                            @csrf
+                            <div class="single-contact-form">
+                                <input type="text" name="name" value="{{old('name')}}" placeholder="Name">
+                                @error('name')
+                                <span class="text-danger">{{$message}}</span>
+                                @enderror
+                            </div>
+                            <div class="single-contact-form space-between">
+                                <input type="email" name="email" value="{{old('email')}}" placeholder="Email">
+                                <input type="text" name="mobile" value="{{old('mobile')}}" placeholder="Mobile">
+                            </div>
+                            <div class="single-contact-form space-between">
+                                @error('email')
+                                <span class="text-danger">{{$message}}</span>
+                                @enderror
+                                @error('mobile')
+                                <span class="text-danger">{{$message}}</span>
+                                @enderror
+                            </div>
+                            <div class="single-contact-form">
+                                {!! Form::label('chalet_id', 'chalet_id') !!}
+                                {!! Form::select('chalet_id', ['' => '---'] + $chalets->toArray(), old('chalet_id'), ['class' => 'form-control']) !!}
+                                @error('chalet_id')<span class="text-danger">{{ $message }}</span>@enderror
+                            </div>
+                            <div class="single-contact-form space-between">
+                                <input type="date" name="cin" value="{{old('cin')}}">
+                                <input type="date" name="cout" value="{{old('cout')}}">
+                            </div>
+                            <div class="single-contact-form space-between">
+                                @error('cin')
+                                <span class="text-danger">{{$message}}</span>
+                                @enderror
+                                @error('cout')
+                                <span class="text-danger">{{$message}}</span>
+                                @enderror
+                            </div>
+                            <div class="contact-btn">
+                                <button type="submit">Reserve</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-12 md-mt-40 sm-mt-40">
+                    <div class="wn__address">
+                        <h2 class="contact__title">Get office info.</h2>
+                        <p>Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum
+                            est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum
+                            formas humanitatis per seacula quarta decima et quinta decima. </p>
+                        <div class="wn__addres__wreapper">
+
+                            <div class="single__address">
+                                <i class="icon-location-pin icons"></i>
+                                <div class="content">
+                                    <span>address:</span>
+                                    <p>666 5th Ave New York, NY, United</p>
+                                </div>
+                            </div>
+
+                            <div class="single__address">
+                                <i class="icon-phone icons"></i>
+                                <div class="content">
+                                    <span>Phone Number:</span>
+                                    <p>716-298-1822</p>
+                                </div>
+                            </div>
+
+                            <div class="single__address">
+                                <i class="icon-envelope icons"></i>
+                                <div class="content">
+                                    <span>Email address:</span>
+                                    <p>716-298-1822</p>
+                                </div>
+                            </div>
+
+                            <div class="single__address">
+                                <i class="icon-globe icons"></i>
+                                <div class="content">
+                                    <span>website address:</span>
+                                    <p>716-298-1822</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
