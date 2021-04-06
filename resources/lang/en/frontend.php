34a08f351385d4056c1fd2efd99fc677eac9a947
shujaa1419 <shjoo1419@gmail.com>
2021-04-06 15:49:26 +0300

    Translating that website

--- resources/lang/en/frontend.php
+++ resources/lang/en/frontend.php
@@ -10,7 +10,11 @@ return [
     'Your email address will not be published.Required fields are marked' => 'Your email address will not be published.Required fields are marked',
     'Hello' => 'Hello',
     'Get in touch' => 'Get in touch',
-    
+    'Send Message' => 'Send Message',
+    'Get office info.' => 'Get office info.',
+    'address:' => 'address:',
+    '2333 al bashur - al hamra RIYADH , SA, Kingdom of Saudi Arabia' => '2333 al bashur - al hamra RIYADH , SA, Kingdom of Saudi Arabia',
+    'Phone Number:' => 'Phone Number:',
     
 
 
